a6a61017bf5d6d562f938cc8fda395090ce21de9
Keith Clark <geauxlsutigers24@gmail.com>
2016-10-25 15:02:47 +0000

    migrated

--- musicDB/db/schema.rb
+++ musicDB/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025141151) do
+ActiveRecord::Schema.define(version: 20161025142102) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.string   "record_label"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "instrument"
   end
 
 end
