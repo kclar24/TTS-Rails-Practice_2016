07bdbb62a97e3f013d2872aedf11fcb69397b102
Keith Clark <geauxlsutigers24@gmail.com>
2016-10-26 15:09:50 +0000

    rails notes

---
+++ railnotes.rb
@@ -0,0 +1,22 @@
+# rake db:migrate
+# when you add a new category, update the index and forms, also dont forget to whitelist the category in your controller.
+#   _____
+#
+# To install gems use gemfile
+#
+
+# 3 areas: development, production, testing,
+# __________
+#
+# SQL Active Record code
+# Artist.where(name: "Taylor Swift")
+# Artist.all
+# Artist.new
+# Artist.new(name: "Big Sean", record_label: "Def Jam Records")
+#
+# CRUD
+# Create == new
+# Read == all, first, last, where, find, find_by, etc
+# Update == save, Update
+# Delete == destroy
+# _____________
