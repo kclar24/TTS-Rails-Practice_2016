cf6b13140332ee55c822e594c5cccf8efedb8d5f
Keith Clark <geauxlsutigers24@gmail.com>
2016-10-26 15:53:00 +0000

    added new dataset, songs. created associations with artist.

--- musicDB/db/schema.rb
+++ musicDB/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025142102) do
+ActiveRecord::Schema.define(version: 20161026152157) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20161025142102) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "instrument"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
 end
