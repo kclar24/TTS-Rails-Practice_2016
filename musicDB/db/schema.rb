17faef575633efce046cf7461940630896a6e9cb
Keith Clark <geauxlsutigers24@gmail.com>
2016-10-27 13:30:44 +0000

    migrated genre

--- musicDB/db/schema.rb
+++ musicDB/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026152157) do
+ActiveRecord::Schema.define(version: 20161027022354) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20161026152157) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "instrument"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "songs", force: :cascade do |t|
